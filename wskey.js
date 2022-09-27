6daa500fb335a345f4019e9b29156199b76ba27d
wannian20 <59636417+wannian20@users.noreply.github.com>
2022-09-27 12:49:03 +0800

    Update wskey.js

--- wskey.js
+++ wskey.js
@@ -10,7 +10,7 @@ var CookieValue = ck.match(/wskey=.+?;/)
 
 data=JSON.parse($response.body)
     data = data.userInfoSns.unickName
-    url = '%E4%BD%99%E6%B8%A9wskey%E8%8E%B7%E5%8F%96%E6%8F%90%E9%86%92%E6%82%A8'
+    url = '%E6%99%9A%E5%B9%B4wskey%E8%8E%B7%E5%8F%96%E6%8F%90%E9%86%92%E6%82%A8'
 
 
 
